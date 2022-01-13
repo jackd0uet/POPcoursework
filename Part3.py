e238936ce10b8086703643053f50575a0c4c1986
jackd0uet <72754693+jackd0uet@users.noreply.github.com>
2022-01-13 16:23:57 +0000

    Fixed syntax

--- Part3.py
+++ Part3.py
@@ -1,6 +1,6 @@
-#A program to keep track of stock items and prices in a car accessories shop.
-#PoP Coursework.
-#Jack Douet
+# A program to keep track of stock items and prices in a car accessories shop.
+# PoP Coursework.
+# Jack Douet
 
 
 # Create StockItem Class
@@ -25,14 +25,14 @@ class StockItem():
     def getQuantity(self):
         return self.__quantity
 
-    def setQuantity(self,value):
-        self.__quantity = value
+    def setQuantity(self, quantity):
+        self.__quantity = quantity
 
     def getPriceNoVAT(self):
         return self.__price
 
-    def setPrice(self,value):
-        self.__price = value
+    def setPrice(self, price):
+        self.__price = price
 
     def getStockName(self):
         return "Unknown stock name."
@@ -43,61 +43,62 @@ class StockItem():
     def getVAT(self):
         return "17.5"
 
-    #Define a function for calculating the price of the object with the VAT rate that has been entered above.
+    # Define a function for calculating the price of the object with the VAT rate that has been entered above.
     def getPriceWithVAT(self):
-        x = self.__price +(0.175 * self.__price)
+        x = self.__price + (0.175 * self.__price)
         x = round(x, 2)
         return x
 
-    
     # Define function for increasing the amount of stock for an individual object of the class.
-    def increaseStock(self,value):
+
+    def increaseStock(self, stock):
         # Create a global noError variable for use later in the program for better user experience.
         global noError
         # Use if statements to check whether the value entered is valid and print an error if it is not.
-        if value < 1:
-            print("ERROR: Value entered is too low, please try again with a value of one or higher!\n")
+        if stock < 1:
+            print(
+                "ERROR: Value entered is too low, please try again with a value of one or higher!\n")
             # Set noError to false if the value entered is invalid.
             noError = False
 
-        elif self.__quantity + value > 100:
+        elif self.__quantity + stock > 100:
             print("ERROR: Cannot have stock over 100 for this item!\n")
             # Set noError to false if the value entered is invalid.
             noError = False
 
         # Otherwise set the quantity to the original quantity plus the user's entered value.
         else:
-            self.__quantity = self.__quantity + value
-
+            self.__quantity = self.__quantity + stock
 
     # Define function for selling stock for an individual object of the class.
-    def sellStock(self,value):
+
+    def sellStock(self, stock):
         # Again define the global variable so that it can be accessed from outside this function creation.
         global noError
         # Use if statements to check whether the value entered is valid and print an error if it is not.
-        if value < 1:
+        if stock < 1:
             print("ERROR: Cannot sell less than one of an item!\n")
             # Set noError to false if the value entered is invalid.
             noError = False
 
-        elif value > self.__quantity:
+        elif stock > self.__quantity:
             print("ERROR: Cannot sell more stock than is available!\n")
             # Set noError to false if the value entered is invalid.
             noError = False
 
         # Otherwise set the quantity of the object to the original quantity minus the user's entered value.
         else:
-            self.__quantity = self.__quantity - value
+            self.__quantity = self.__quantity - stock
 
-    # Define string method so when the print function is called on an object of this class a formatted string will be printed.        
+    # Define string method so when the print function is called on an object of this class a formatted string will be printed.
     def __str__(self):
-        return "\nPrinting item stock information: " "\nStock Category: " + self.stockCategory + "\nStock Type: " + self.getStockName() + "\nDescription: " + self.getStockDescription() + "\nStockCode: " + str(self.getStockCode()) + "\nPriceWithoutVat: " +str(self.getPriceNoVAT()) + "\nPriceWithVat: " +str(self.getPriceWithVAT()) + "\nTotal units in stock: " +str(self.getQuantity()) + "\n"
+        return "\nPrinting item stock information: " "\nStock Category: " + self.stockCategory + "\nStock Type: " + self.getStockName() + "\nDescription: " + self.getStockDescription() + "\nStockCode: " + str(self.getStockCode()) + "\nPriceWithoutVat: " + str(self.getPriceNoVAT()) + "\nPriceWithVat: " + str(self.getPriceWithVAT()) + "\nTotal units in stock: " + str(self.getQuantity()) + "\n"
 
 
 # Define a new class that inherits from the StockItem parent class.
 class NavSys(StockItem):
     # Inherit stockCode, quantity and price from the StockItem parent and create a new variable indicating a new class of item.
-    def __init__(self,stockCode, quantity, price, navSysBrand):
+    def __init__(self, stockCode, quantity, price, navSysBrand):
         self.__navSysBrand = navSysBrand
         super().__init__(stockCode, quantity, price)
 
@@ -116,10 +117,10 @@ class NavSys(StockItem):
 
 # Define another new class that inherits from the StockItem parent class.
 class Speaker(StockItem):
-     # Inherit stockCode, quantity and price from the StockItem parent and create a new variable indicating a new class of item.
+    # Inherit stockCode, quantity and price from the StockItem parent and create a new variable indicating a new class of item.
     def __init__(self, stockCode, quantity, price, speakerBrand):
         self.__speakerBrand = speakerBrand
-        super().__init__(stockCode,quantity,price)
+        super().__init__(stockCode, quantity, price)
 
     # Replace the default stockName with Speaker.
     def getStockName(self):
@@ -132,6 +133,7 @@ class Speaker(StockItem):
     # Inherit the string method from parent class and add the brand to the end.
     def __str__(self):
         return super().__str__() + "Brand: " + self.__speakerBrand + "\n"
+
 
 # Define another new class that inherits from the StockItem parent class.
 class AirFreshener(StockItem):
@@ -150,7 +152,8 @@ class AirFreshener(StockItem):
 
     # Inherit the string method from parent class and add the brand to the end.
     def __str__(self):
-        return super().__str__() +"Brand: " + self.__airFreshenerBrand + "\n"
+        return super().__str__() + "Brand: " + self.__airFreshenerBrand + "\n"
+
 
 # Define another new class that inherits from the StockItem parent class.
 class HandsFree(StockItem):
@@ -169,7 +172,8 @@ class HandsFree(StockItem):
 
     # Inherit the string method from parent class and add the brand to the end.
     def __str__(self):
-        return super().__str__() +"Brand: " + self.__handsFreeBrand + "\n"
+        return super().__str__() + "Brand: " + self.__handsFreeBrand + "\n"
+
 
 # Define main function.
 def main():
@@ -180,13 +184,13 @@ def main():
     handsFree = HandsFree("HF100", 25, 59.99, "Scosche")
 
     # Create a variable and set to true for use with a while loop.
-    runProgram = True 
+    runProgram = True
 
     # Create array to store created objects.
     itemsInStock = [navsys, speaker, airFreshener, handsFree]
 
     # While loop for user interface.
-    while runProgram: 
+    while runProgram:
 
         # For loop to show all of the available objects.
         for stockitem in (navsys, speaker, airFreshener, handsFree):
@@ -196,18 +200,18 @@ def main():
         secondLoop = True
         # While loop to ask user which object they would like to interact with.
         while secondLoop:
-            # Capture the input as integer in a variable.  
-            stockItemInput = int(input("Which stock item would you like to interact with? : "))
-
+            # Capture the input as integer in a variable.
+            stockItemInput = int(
+                input("Which stock item would you like to interact with? : "))
             # If the user inputs a number that is within the length of the itemsInStock array and is not zero break out of the while loop.
             if stockItemInput <= len(itemsInStock) and stockItemInput != 0:
                 break
             # If the user enters anything else, print an error and continue to the top of the loop.
             else:
                 print("ERROR: This is not a valid option!")
-                continue 
+                continue
 
-        # Take one of the users entered input so that it correctly matches up with the index in the array for that object. 
+        # Take one of the users entered input so that it correctly matches up with the index in the array for that object.
         stockItemInput -= 1
 
         # Print out the users selected object from the array of objects.
@@ -219,7 +223,6 @@ def main():
         print("[2] Set Price")
         print("[3] Increase Stock")
         print("[4] Sell Stock")
-        
 
         # Variable for initialising another while loop.
         thirdLoop = True
@@ -228,40 +231,40 @@ def main():
             # Set noError to true in case it is set to false by an incorrect increase or sell of stock.
             noError = True
             # Ask user what they would like to do and capture their input.
-            userActionInput = int(input("What would you like to do to this stockitem?: "))
-
+            userActionInput = int(
+                input("What would you like to do to this stockitem?: "))
             # If user picks option 1, run function for setting quantity.
             if userActionInput == 1 and userActionInput != 0:
                 # Ask the user what they would like to set the quantity as and capture as an integer.
-                newQuantity = int(input("What would you like to set the quantity to?: "))
+                newQuantity = int(
+                    input("What would you like to set the quantity to?: "))
                 # Set the quantity to entered integer and break out of loop.
                 itemsInStock[stockItemInput].setQuantity(newQuantity)
                 break
-
             # If user picks option 2, run function for setting price.
             elif userActionInput == 2 and userActionInput != 0:
                 # Ask the user what they would like to set the price to and capture as a double.
-                newPrice = float(input("What would you like to set the new price as?: "))
+                newPrice = float(
+                    input("What would you like to set the new price as?: "))
                 # Set the quantity to entered double and break out of loop.
                 itemsInStock[stockItemInput].setPrice(newPrice)
                 break
-
             # If user picks option 3, run function for increasing stock.
             elif userActionInput == 3 and userActionInput != 0:
                 # Ask the user how much they would like to increase the stock by and capture as int.
-                userIncreaseStock = int(input("How much would you like to increase the stock by?: "))
+                userIncreaseStock = int(
+                    input("How much would you like to increase the stock by?: "))
                 # Use the entered int in the function to increase the stock and break out of loop.
                 itemsInStock[stockItemInput].increaseStock(userIncreaseStock)
                 break
-
             # If user picks option 4, run function for selling stock.
             elif userActionInput == 4 and userActionInput != 0:
                 # Ask the user how much stock they would like to sell and capture as int.
-                userSellStock = int(input("How much stock would you like to sell?: "))
+                userSellStock = int(
+                    input("How much stock would you like to sell?: "))
                 # Use the entered int in the function to sell the stock and break out of loop.
                 itemsInStock[stockItemInput].sellStock(userSellStock)
                 break
-
             # Otherwise warn the user that they have entered an invalid option and continue to the top of the loop.
             else:
                 print("ERROR: This is not a valid option!")
@@ -271,9 +274,10 @@ def main():
         if noError == True:
             print("Here is the updated stock item!")
             print(itemsInStock[stockItemInput])
-        
+
         # Ask the user if they would like to run the program again.
-        userInput = input("Would you like to run the program again? (Yes / No)?: ")
+        userInput = input(
+            "Would you like to run the program again? (Yes / No)?: ")
 
         # Use .upper to capitalise the input.
         userInput = userInput.upper()
@@ -285,5 +289,6 @@ def main():
         elif userInput == "YES":
             continue
 
+
 if __name__ == "__main__":
-    main()        
+    main()
